8b7c3be5255cb7cc0ba51b57b41efa262bd8dc47
Jovany Cortez <jovanycortezm@gmail.com>
2023-07-25 20:57:18 -0400

    week8 work

---
+++ 05-JWT-Basics/starter/errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require('./custom-error')
+const BadRequest = require('./bad-request')
+const UndauthenticatedError = require('./unauthenticated')
+
+module.exports = {
+    CustomAPIError,
+    BadRequest,
+    UndauthenticatedError,
+}
